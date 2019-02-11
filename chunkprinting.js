d8d43ce1236f9443e861ada6d73389f302b12b0e
JaffarShah <sjeffshah@gmail.com>
2019-02-11 18:48:07 +0000

    my comments in the file

--- chunkprinting.js
+++ chunkprinting.js
@@ -1,21 +1,20 @@
-var https = require('https');
+var https = require('https'); // We need to set up https here to require data in cmd
 
 
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
-    host: 'sytantris.github.io',
+    host: 'sytantris.github.io',// can be any website or link, but in our case it is already given a link
     path: '/http-examples/step1.html'
   };
 /* Add your code here */
 https.get(requestOptions, function (response) {
 
-  response.setEncoding('utf8');
+  response.setEncoding('utf8');// is a variable width character encoding: https://en.wikipedia.org/wiki/UTF-8
 
 response.on('data', function(data) {
 
   console.log(data);
-
 
 });
 
@@ -23,8 +22,7 @@ response.on('end', function() {
   console.log('Response stream complete.');
 });
 
-
 });
 
 }
-console.log(getAndPrintHTMLChunks());
+console.log(getAndPrintHTMLChunks());// we need to console.log to call in the data we are requiring
